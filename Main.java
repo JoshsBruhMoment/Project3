825f087ce57cc1ce67ddd0b93ae0832c96a30abe
JoshsBruhMoment <joshua.aldahondo@patelhs.org>
2020-11-10 17:00:45 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    int[] seatNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+    String[] names = new String[15];
+
+    Scanner myObj = new Scanner(System.in);
+
+    for (int i = 0; i < seatNumbers.length; i++) {
+
+      System.out.println("what is your name? ");
+      names[i] = myObj.nextLine();
+
+    }
+    int i = 0;
+    while (i < seatNumbers.length) {
+      i++;
+      System.out.println(names + " sits in seat " + seatNumbers);
+    }
+
+  }
+}
