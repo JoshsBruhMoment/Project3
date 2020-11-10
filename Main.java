5e21fe0f254126353538ce9068a4c2499fd97469
JoshsBruhMoment <joshua.aldahondo@patelhs.org>
2020-11-10 17:20:49 +0000

    added an extra value to see if the command would work (It made an error)

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    int[] seatNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,  };//seat numbers
+    int[] seatNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,15,16 };//seat numbers
 
     String[] names = new String[15];//makes a string of 15
 
